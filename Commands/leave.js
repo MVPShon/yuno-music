a7400b210bac8c92c4708632310b1517a9e1178c
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-10 18:16:15 -0400

    Update leave.js

--- Commands/leave.js
+++ Commands/leave.js
@@ -2,5 +2,9 @@ const Discord = require("discord.js");
 const ytdl = require('ytdl-core');
 const ytsearch = require('youtube-search');
     exports.run = async (musicbot, message, args, prefix, server) => {
+        if(!message.guild.me.voiceChannel) return message.channel.send("I am not in a voice channel!")
+        
+message.guild.me.voiceChannel.leave();
 
+    message.channel.send("Left the voice channel!");
     }
