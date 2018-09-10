9c362649470fdd81416683ecc4750380aec012fa
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-10 16:47:32 -0400

    Update play.js

--- Commands/play.js
+++ Commands/play.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 var search = require('youtube-search');
 
-exports.run = async (bot, message, args, prefix, server) => {
+exports.run = async (musicbot, message, args, prefix, server) => {
 
 if(!message.member.voiceChannel) return message.channel.send(":x: | Sorry! Please join a Voice Channel.");
 
@@ -33,10 +33,37 @@ function play(connection, message) {
     if(err) return console.log(err);
    
     let resOne = results[0];
+    let resTwo = results[1];
+    let resThree = results[2];
+    let resFour = results[3];
+    let resFive = results[4];
 
-server.queue.push(resOne.link)
+    let embed = new Discord.RichEmbed()
+    .setColor(0xffc07c)
+    .setDescription(`♥ **${resOne.title}**\n💛 ${resTwo.title}\n💚 ${resThree.title}\n💙 ${resFour.title}\n💜 ${resFive.title}`)
+    message.channel.send(embed).then(embeds => {
+        embeds.react("♥").then(async r => {
+            await embeds.react("💛");
+            await embeds.react("💚");
+            await embeds.react("💙");
+            await embeds.react("💜");
 
-  let info = await ytdl.getInfo(resOne.link);
+            const songOne = (reaction, user) => reaction.emoji.name === "♥" && user.id === message.author.id;
+            const songTwo = (reaction, user) => reaction.emoji.name === "💛" && user.id === message.author.id;
+            const songThree = (reaction, user) => reaction.emoji.name === "💚" && user.id === message.author.id;
+            const songFour = (reaction, user) => reaction.emoji.name === "💙" && user.id === message.author.id;
+            const songFive = (reaction, user) => reaction.emoji.name === "💜" && user.id === message.author.id;
+
+            const one = embeds.createReactionCollector(songOne, { time: 60000 });
+            const two = embeds.createReactionCollector(songTwo, { time: 60000 });
+            const three = embeds.createReactionCollector(songThree, { time: 60000 });
+            const four = embeds.createReactionCollector(songFour, { time: 60000 });
+            const five = embeds.createReactionCollector(songFive, { time: 60000 });
+
+            one.on('collect', async r => {
+                server.queue.push(resOne.link)
+                embeds.delete();
+                let info = await ytdl.getInfo(resOne.link);
                 let playEmbed = new Discord.RichEmbed()
                 .setColor(0xffc07c)
                 .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
@@ -45,6 +72,64 @@ server.queue.push(resOne.link)
                 if(!message.guild.voiceConnection) await message.member.voiceChannel.join().then(function(connection) {
                     play(connection, message);
                   });
+              })
+              two.on('collect', async r => {
+                server.queue.push(resTwo.link)
+                embeds.delete();
+                let info = await ytdl.getInfo(resTwo.link);
+                let playEmbed = new Discord.RichEmbed()
+                .setColor(0xffc07c)
+                .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
+                .setThumbnail(info.thumbnail_url)
+                message.channel.send(playEmbed)
+                if(!message.guild.voiceConnection) await message.member.voiceChannel.join().then(function(connection) {
+                    play(connection, message);
+                  });
+              })
+              three.on('collect', async r => {
+                server.queue.push(resThree.link)
+                embeds.delete();
+                let info = await ytdl.getInfo(resThree.link);
+                let playEmbed = new Discord.RichEmbed()
+                .setColor(0xffc07c)
+                .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
+                .setThumbnail(info.thumbnail_url)
+                message.channel.send(playEmbed)
+                if(!message.guild.voiceConnection) await message.member.voiceChannel.join().then(function(connection) {
+                    play(connection, message);
+                  });
+              })
+              four.on('collect', async r => {
+                server.queue.push(resFour.link)
+                embeds.delete();
+                let info = await ytdl.getInfo(resFour.link);
+                let playEmbed = new Discord.RichEmbed()
+                .setColor(0xffc07c)
+                .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
+                .setThumbnail(info.thumbnail_url)
+                message.channel.send(playEmbed)
+                if(!message.guild.voiceConnection) await message.member.voiceChannel.join().then(function(connection) {
+                    play(connection, message);
+                  });
+              })
+              five.on('collect', async r => {
+                server.queue.push(resFive.link)
+                embeds.delete();
+                let info = await ytdl.getInfo(resFive.link);
+                let playEmbed = new Discord.RichEmbed()
+                .setColor(0xffc07c)
+                .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
+                .setThumbnail(info.thumbnail_url)
+                message.channel.send(playEmbed)
+                if(!message.guild.voiceConnection) await message.member.voiceChannel.join().then(function(connection) {
+                    play(connection, message);
+                  });
+              })
 
-});
+        });
+    });
+  });
+
+
+
 }
