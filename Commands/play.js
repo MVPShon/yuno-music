737cfd587dd8ba64c3b0eba21efda205b073d971
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-10 16:56:42 -0400

    Update play.js

--- Commands/play.js
+++ Commands/play.js
@@ -8,7 +8,7 @@ if(!message.member.voiceChannel) return message.channel.send(":x: | Sorry! Pleas
 
 if(message.guild.me.voiceChannel) return message.channel.send(":x: | Sorry! I'm already playing in a channel.");
 
-if(!args[0]) return message.channel.send(":x: | Sorry! Please provide a Search String or URL.");
+if(!args[0]) return message.channel.send("I can't search for *nothing*!");
 
 function play(connection, message) {
      
@@ -19,7 +19,7 @@ function play(connection, message) {
      server.dispatcher.on("end", function() {
      if(server.queue[0]) return play(connection, message);
        else connection.disconnect();
-       message.channel.send(":dash: | Queue is empty.");
+       message.channel.send("The queue is empty!");
      
      });
    }
@@ -39,20 +39,19 @@ function play(connection, message) {
     let resFive = results[4];
 
     let embed = new Discord.RichEmbed()
-    .setColor(0xffc07c)
-    .setDescription(`♥ **${resOne.title}**\n💛 ${resTwo.title}\n💚 ${resThree.title}\n💙 ${resFour.title}\n💜 ${resFive.title}`)
+    .setDescription(`:one: **${resOne.title}**\n:two: ${resTwo.title}\n:three: ${resThree.title}\n:four: ${resFour.title}\n:five: ${resFive.title}`)
     message.channel.send(embed).then(embeds => {
-        embeds.react("♥").then(async r => {
-            await embeds.react("💛");
-            await embeds.react("💚");
-            await embeds.react("💙");
-            await embeds.react("💜");
+        embeds.react(":one:").then(async r => {
+            await embeds.react(":two:");
+            await embeds.react(":three:");
+            await embeds.react(":four:");
+            await embeds.react(":five:");
 
-            const songOne = (reaction, user) => reaction.emoji.name === "♥" && user.id === message.author.id;
-            const songTwo = (reaction, user) => reaction.emoji.name === "💛" && user.id === message.author.id;
-            const songThree = (reaction, user) => reaction.emoji.name === "💚" && user.id === message.author.id;
-            const songFour = (reaction, user) => reaction.emoji.name === "💙" && user.id === message.author.id;
-            const songFive = (reaction, user) => reaction.emoji.name === "💜" && user.id === message.author.id;
+            const songOne = (reaction, user) => reaction.emoji.name === ":one:" && user.id === message.author.id;
+            const songTwo = (reaction, user) => reaction.emoji.name === ":two:" && user.id === message.author.id;
+            const songThree = (reaction, user) => reaction.emoji.name === ":three:" && user.id === message.author.id;
+            const songFour = (reaction, user) => reaction.emoji.name === ":four:" && user.id === message.author.id;
+            const songFive = (reaction, user) => reaction.emoji.name === ":five:" && user.id === message.author.id;
 
             const one = embeds.createReactionCollector(songOne, { time: 60000 });
             const two = embeds.createReactionCollector(songTwo, { time: 60000 });
@@ -65,7 +64,6 @@ function play(connection, message) {
                 embeds.delete();
                 let info = await ytdl.getInfo(resOne.link);
                 let playEmbed = new Discord.RichEmbed()
-                .setColor(0xffc07c)
                 .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
                 .setThumbnail(info.thumbnail_url)
                 message.channel.send(playEmbed)
@@ -78,7 +76,6 @@ function play(connection, message) {
                 embeds.delete();
                 let info = await ytdl.getInfo(resTwo.link);
                 let playEmbed = new Discord.RichEmbed()
-                .setColor(0xffc07c)
                 .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
                 .setThumbnail(info.thumbnail_url)
                 message.channel.send(playEmbed)
@@ -91,7 +88,6 @@ function play(connection, message) {
                 embeds.delete();
                 let info = await ytdl.getInfo(resThree.link);
                 let playEmbed = new Discord.RichEmbed()
-                .setColor(0xffc07c)
                 .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
                 .setThumbnail(info.thumbnail_url)
                 message.channel.send(playEmbed)
@@ -104,7 +100,6 @@ function play(connection, message) {
                 embeds.delete();
                 let info = await ytdl.getInfo(resFour.link);
                 let playEmbed = new Discord.RichEmbed()
-                .setColor(0xffc07c)
                 .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
                 .setThumbnail(info.thumbnail_url)
                 message.channel.send(playEmbed)
@@ -117,7 +112,6 @@ function play(connection, message) {
                 embeds.delete();
                 let info = await ytdl.getInfo(resFive.link);
                 let playEmbed = new Discord.RichEmbed()
-                .setColor(0xffc07c)
                 .setAuthor(`Added ${info.title} to the queue`, info.author.avatar)
                 .setThumbnail(info.thumbnail_url)
                 message.channel.send(playEmbed)
