2de331fdf90342fa1f10211a34a65e152cc106b3
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-12 14:39:29 -0400

    Update play.js

--- Commands/play.js
+++ Commands/play.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 var search = require('youtube-search');
 
-exports.run = async (musicbot, message, args, prefix, server) => {
+exports.run = async (musicbot, message, args, prefix, server, names) => {
 
 if(!message.member.voiceChannel) return message.channel.send("You're not in a voice channel!");
 
