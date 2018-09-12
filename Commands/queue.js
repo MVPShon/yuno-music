c3b88de88d5435c15580935027655dc29ccfe68c
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-12 14:53:16 -0400

    Update queue.js

--- Commands/queue.js
+++ Commands/queue.js
@@ -3,7 +3,13 @@ const ytdl = require("ytdl-core");
 var search = require('youtube-search');
 
 exports.run = async (musicbot, message, args, prefix, server, names) => {
+  
+  if(!names.names) return message.channel.send(":x: The queue is empty!")
 
-console.log(names.names)
+let embed = new Discord.RichEmbed()
+.setColor(0xff6464)
+.setTitle(`**${message.guild.name}**'s Song Queue`)
+.setDescription(names.names)
+message.channel.send(embed)
 
 }
