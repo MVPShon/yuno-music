4351542ad078d8759120c935f336a8a0140f2aed
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-12 14:22:50 -0400

    Delete search.js

--- Commands/search.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-const ytdl = require('ytdl-core');
-const ytsearch = require('youtube-search');
-    exports.run = async (musicbot, message, args, prefix, server) => {
-
-    }
