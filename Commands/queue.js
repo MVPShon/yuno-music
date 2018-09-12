3b25b285c755a711dca854cfffbfe34e50c4148f
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-12 14:24:08 -0400

    Create queue.js

---
+++ Commands/queue.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+var search = require('youtube-search');
+
+exports.run = async (musicbot, message, args, prefix, server) => {
+
+console.log(server.queue)
+
+}
