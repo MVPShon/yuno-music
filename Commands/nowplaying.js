409b0bcbad7c74f5ced65d246f623d3618bff9ad
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-13 11:16:59 -0400

    Update nowplaying.js

--- Commands/nowplaying.js
+++ Commands/nowplaying.js
@@ -4,5 +4,11 @@ var search = require('youtube-search');
 
 exports.run = async (musicbot, message, args, prefix, server, names) => {
   if (names.names == "undefined") return message.channel.send("No song is currently playing!")
-message.channel.send(`Now Playing: ${names.names[0]}`);
+          let songInfo = await ytdl.getInfo(server.queue[0]);
+          let embed = new Discord.RichEmbed()
+          .setTitle(songInfo.title)
+          .setDescription((songInfo.description).slice(0,100))
+          .setThumbnail(songInfo.thumbnail_url)
+          .setFooter("Published by: " + songInfo.author.user)
+          message.channel.send(embed);
 }
