642a6cb4b7325e00585b9d7ffe98424b3ab542bf
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-13 11:31:13 -0400

    Update nowplaying.js

--- Commands/nowplaying.js
+++ Commands/nowplaying.js
@@ -1,14 +1,7 @@
 const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 var search = require('youtube-search');
-
-exports.run = async (musicbot, message, args, prefix, server, names) => {
-  if (names.names == "undefined") return message.channel.send("No song is currently playing!")
-          let songInfo = await ytdl.getInfo(server.queue[0]);
-          let embed = new Discord.RichEmbed()
-          .setTitle(songInfo.title)
-          .setDescription((songInfo.description).slice(0,100))
-          .setThumbnail(songInfo.thumbnail_url)
-          .setFooter("Published by: " + songInfo.author.user)
-          message.channel.send(embed);
+ exports.run = async (musicbot, message, args, prefix, server, names) => {
+  if (names.names[0] == "undefined") return message.channel.send("No song is currently playing!")
+message.channel.send(`Now Playing: ${names.names[0]}`);
 }
