acbac1e10403b8ad2821ef362ba9ebf63d9de469
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-13 11:04:21 -0400

    Create song.js

---
+++ Commands/song.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+var search = require('youtube-search');
+
+exports.run = async (musicbot, message, args, prefix, server, names) => {
+  if (names.names == "undefined") return message.channel.send("No song is currently playing!")
+message.channel.send(`Now Playing: ${names.names[0]}`);
+}
