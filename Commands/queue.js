ac62a4868ba3f7fc36601aabb82b0b39eb53406b
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-12-26 02:02:38 -0500

    Update queue.js

--- Commands/queue.js
+++ Commands/queue.js
@@ -7,7 +7,7 @@ exports.run = async (musicbot, message, args, prefix, server, names) => {
   if(names.names == "undefined") return message.channel.send(":x: The queue is empty!")
 
 let embed = new Discord.RichEmbed()
-.setColor(0xff6464)
+.setColor(0x8600b3)
 .setTitle(`**${message.guild.name}**'s Song Queue`)
 .setDescription("Currently Playing: " +names.names)
 message.channel.send(embed)
